e7b8a6dd7a63e389a1ca4bb7264cb0cbe8a8e1c5
haneric <haneric1986@gmail.com>
2017-04-25 15:47:37 +0800

    use promise

--- js/rate-service-mock.js
+++ js/rate-service-mock.js
@@ -1,12 +1,19 @@
 let rates = [
   {
-    "name": "55 years fixed",
+    "name": "55 years fixed test",
     "rate": "4",
     "years": "12"
-  };
+  },
   {
-    "name": "33 years fixed",
+    "name": "33 years fixed test",
     "rate": "42",
     "years": "32"
   }
 ];
+
+export let findAll = () => new Promise((resolve, reject) => {
+  if (rates) {
+    resolve(rates);
+  };
+  rejects("No rates");
+});
--- js/ratefinder.js
+++ js/ratefinder.js
@@ -1,3 +1,5 @@
+import * as service from './rate-service-mock.js';
+
 let url = "rates.json";
 
 fetch(url)
@@ -6,5 +8,13 @@ fetch(url)
     let html = '';
     rates.forEach(rates => html += `<tr><td>${rates.name}</td><td>${rates.years}</td><td>${rates.rate}</td></tr>`);
     document.getElementById('rates').innerHTML = html;
+  })
+  .catch(e => console.log(e));
+
+service.findAll()
+  .then(rates => {
+    let html = '';
+    rates.forEach(rates => html += `<tr><td>${rates.name}</td><td>${rates.years}</td><td>${rates.rate}</td></tr>`);
+    document.getElementById('rates-1').innerHTML = html;
   })
   .catch(e => console.log(e));

6c2ff80df71ce28c7488a31db67750998c2d3f2f
haneric <haneric1986@gmail.com>
2017-04-25 13:39:22 +0800

    use template literals

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const sayHi = () => {
   let firstName = document.getElementById('first-name').value;
   let lastName = document.getElementById('last-name').value;
 
-  let html = 'Hello <em>' + firstName + '</em> ' + lastName;
+  let html = `Hello <em>${firstName}</em> ${lastName}`;
 
   document.getElementById('result').innerHTML = html;
 }

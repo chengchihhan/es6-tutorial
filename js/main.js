a30db273875798621df6b3bf41a616f18c28a11d
haneric <haneric1986@gmail.com>
2017-04-25 11:36:05 +0800

    modify indents

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function sayHi() {
-    var firstName = document.getElementById('first-name').value;
-    var lastName = document.getElementById('last-name').value;
+  var firstName = document.getElementById('first-name').value;
+  var lastName = document.getElementById('last-name').value;
 
-    var html = 'Hello <em>' + firstName + '</em> ' + lastName;
+  var html = 'Hello <em>' + firstName + '</em> ' + lastName;
 
-    document.getElementById('result').innerHTML = html;
+  document.getElementById('result').innerHTML = html;
 }
 
 document.getElementById('say').addEventListener('click', sayHi, false);

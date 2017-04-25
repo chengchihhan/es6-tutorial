77afa95d748a26a02bd6df4a23a2d94859f59c4f
haneric <haneric1986@gmail.com>
2017-04-25 14:49:18 +0800

    add ratefinder.js

---
+++ js/ratefinder.js
@@ -0,0 +1,10 @@
+let url = "rates.json";
+
+fetch(url)
+  .then(respose => respose.json())
+  .then(rates => {
+    let html = '';
+    rates.forEach(rates => html += `<tr><td>${rate.name}</td><td>${rates.years}</td><td>${rates.rate}</td></tr>`);
+    document.getElementById('rates').innerHTML = html;
+  })
+  .catch(e => console.log(e));

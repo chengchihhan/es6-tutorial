151faf76f210367cba7bb76d1ddb323d5105a026
haneric <haneric1986@gmail.com>
2017-04-25 14:58:49 +0800

    modify rate to rates

--- js/ratefinder.js
+++ js/ratefinder.js
@@ -4,7 +4,7 @@ fetch(url)
   .then(respose => respose.json())
   .then(rates => {
     let html = '';
-    rates.forEach(rates => html += `<tr><td>${rate.name}</td><td>${rates.years}</td><td>${rates.rate}</td></tr>`);
+    rates.forEach(rates => html += `<tr><td>${rates.name}</td><td>${rates.years}</td><td>${rates.rate}</td></tr>`);
     document.getElementById('rates').innerHTML = html;
   })
   .catch(e => console.log(e));

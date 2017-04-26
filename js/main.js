dc1f315d8d98fe2485b4f77367fc30b41e3c0998
haneric <haneric1986@gmail.com>
2017-04-26 10:33:35 +0800

    test async api

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import * as mock from './test';
+
 let url = "info.json";
 
 fetch(url)
@@ -6,5 +8,13 @@ fetch(url)
     let html = "";
     info.forEach(info => html += `<tr><td>${info.name}</td><td>${info.sex}</td><td>${info.years}</td></tr>`);
     document.getElementById('info').innerHTML = html;
+  })
+  .catch(e => console.log(e));
+
+mock.findAll()
+  .then(test => {
+    let html = "";
+    test.forEach(test => html += `<tr><td>${test.name}</td><td>${test.sex}</td><td>${test.years}</td></tr>`);
+    document.getElementById('test').innerHTML = html;
   })
   .catch(e => console.log(e));

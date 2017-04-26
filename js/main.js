1dd8eab0152c41cad3afbfddb67218682c98d56b
haneric <haneric1986@gmail.com>
2017-04-26 18:50:45 +0800

    add event listener

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 import sayHi from './sayHi';
+import Space from './space';
 
 document.getElementById('say').addEventListener('click', sayHi, false);
+
+document.getElementById('calcBtn').addEventListener('click', () => {
+  let x = document.getElementById('x').value;
+  let y = document.getElementById('y').value;
+  let z = document.getElementById('z').value;
+  let space = new Space(x, y, z);
+  document.getElementById('volumeEqual').innerHTML = x * y * z;
+});

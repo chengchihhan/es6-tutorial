d5f85d37bf248734a69517c360b92475adce40b0
haneric <haneric1986@gmail.com>
2017-04-28 00:01:55 +0800

    use async/await

--- js/main.js
+++ js/main.js
@@ -2,14 +2,17 @@ import * as mock from './test';
 
 let url = "info.json";
 
-fetch(url)
-  .then(response => response.json())
-  .then(info => {
+(async function fetchAsync() {
+  try {
+    let response = await fetch(url);
+    let info = await response.json();
     let html = "";
     info.forEach(info => html += `<tr><td>${info.name}</td><td>${info.sex}</td><td>${info.years}</td></tr>`);
     document.getElementById('info').innerHTML = html;
-  })
-  .catch(e => console.log(e));
+  } catch(e) {
+    console.log("ohohoh nooooooo");
+  }
+})();
 
 mock.findAll()
   .then(test => {

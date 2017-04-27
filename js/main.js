8dd0dcb439cd3d2d7d2bde0991b3ad0e4705c02a
haneric <haneric1986@gmail.com>
2017-04-27 12:08:06 +0800

    use destructuring

--- js/main.js
+++ js/main.js
@@ -10,3 +10,20 @@ document.getElementById('calcBtn').addEventListener('click', () => {
   let space = new Space(x, y, z);
   document.getElementById('volumeEqual').innerHTML = space.volume;
 });
+
+
+function config() {
+  let length = document.getElementById('x').value;
+  let width = document.getElementById('y').value;
+
+  return {
+    length,
+    width
+  }
+}
+
+let {length, width} = config();
+
+document.getElementById('calcBtn').addEventListener('click', () => {
+  document.getElementById('square').innerHTML = length * width;
+});

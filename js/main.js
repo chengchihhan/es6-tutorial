00a42135c9f37a270a806d2641b058ac71900bfd
haneric <haneric1986@gmail.com>
2017-04-29 03:06:49 +0800

    use eslint

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 import * as mock from './test';
 
-let url = "info.json";
+const url = 'info.json';
 
 (async function fetchAsync() {
   try {
-    let response = await fetch(url);
-    let info = await response.json();
-    let html = "";
-    info.forEach(info => html += `<tr><td>${info.name}</td><td>${info.sex}</td><td>${info.years}</td></tr>`);
+    const response = await fetch(url);
+    const info = await response.json();
+    let html = '';
+    info.forEach((info) => {
+      html += `<tr><td>${info.name}</td><td>${info.sex}</td><td>${info.years}</td></tr>`;
+    });
     document.getElementById('info').innerHTML = html;
-  } catch(e) {
-    console.log("ohohoh nooooooo");
   }
-})();
+  catch (e) {
+    console.log('ohohoh nooooooo');
+  }
+}());
 
 mock.findAll()
-  .then(test => {
-    let html = "";
-    test.forEach(test => html += `<tr><td>${test.name}</td><td>${test.sex}</td><td>${test.years}</td></tr>`);
+  .then((test) => {
+    let html = '';
+    test.forEach((test) => {
+      html += `<tr><td>${test.name}</td><td>${test.sex}</td><td>${test.years}</td></tr>`;
+    });
     document.getElementById('test').innerHTML = html;
   })
   .catch(e => console.log(e));
@@ -28,6 +33,6 @@ const promises = [
 ];
 
 Promise.all(promises)
-  .then(promises => {
+  .then((promises) => {
     console.log('value', promises.join('and'));
   });

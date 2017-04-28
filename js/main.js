dbf3849beaa47a1e54c822d2e31253d2dfe22ae8
haneric <haneric1986@gmail.com>
2017-04-29 00:14:56 +0800

    use decorator

--- js/main.js
+++ js/main.js
@@ -1,33 +1,14 @@
-import * as mock from './test';
-
-let url = "info.json";
-
-(async function fetchAsync() {
-  try {
-    let response = await fetch(url);
-    let info = await response.json();
-    let html = "";
-    info.forEach(info => html += `<tr><td>${info.name}</td><td>${info.sex}</td><td>${info.years}</td></tr>`);
-    document.getElementById('info').innerHTML = html;
-  } catch(e) {
-    console.log("ohohoh nooooooo");
+class Cat {
+  @addColor
+  speak() {
+    return `Meow Meow!`;
   }
-})();
+}
 
-mock.findAll()
-  .then(test => {
-    let html = "";
-    test.forEach(test => html += `<tr><td>${test.name}</td><td>${test.sex}</td><td>${test.years}</td></tr>`);
-    document.getElementById('test').innerHTML = html;
-  })
-  .catch(e => console.log(e));
+function addColor(target) {
+  target.color = 'black';
+}
 
-const promises = [
-  new Promise(resolve => setTimeout(resolve, 0, 1)),
-  new Promise(resolve => setTimeout(resolve, 0, 2))
-];
+const a1 = new Cat();
 
-Promise.all(promises)
-  .then(promises => {
-    console.log('value', promises.join('and'));
-  });
+console.log('a1 color is', a1.color);

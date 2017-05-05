d7e93d5ead14d37747053a042c2bbe3112f1c909
chengchihhan <haneric1986@gmail.com>
2017-05-05 15:20:44 +0800

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,12 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: ['babel-polyfill', './js/main.js'],
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    'babel-polyfill',
+    './js/main.js'
+  ],
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'main.bundle.js'
@@ -13,10 +18,11 @@ module.exports = {
         test: /\.js?$/,
         enforce: 'pre',
         exclude: /node_modules/,
-        loader: 'eslint-loader'
-      }
-    ],
-    loaders: [
+        loader: 'eslint-loader',
+        options: {
+          emitError: true,
+        }
+      },
       {
         test: /\.js$/,
         loader: 'babel-loader',
@@ -24,14 +30,29 @@ module.exports = {
           plugins: ['transform-runtime'],
           presets: ['es2015', 'stage-0'],
         },
-        exclude: [
-          path.resolve(__dirname, 'node_modules'),
-        ],
+        exclude: /node_modules/,
       }
     ]
   },
   stats: {
     colors: true
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  devServer: {
+    hot: true,
+    // enable HMR on the server
+
+    contentBase: path.join(__dirname, '/'),
+    // match the output path
+
+    publicPath: '/'
+    // match the output `publicPath`
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    // enable HMR globally
+
+    new webpack.NamedModulesPlugin(),
+    // prints more readable module names in the browser console on HMR updates
+  ],
 };
